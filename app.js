1b06b1418eee0fc96410441f11db159ef4372672
BENAMARA <fahd.benamara@addinn.com>
2024-02-09 23:23:25 +0100

    BackendAdded

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require("express");
+const cors = require("cors"); // Import the cors middleware
 const app = express();
 const logger = require("morgan");
 require("dotenv").config();
 const userRouter = require("./routes/user.router");
 const { Connect } = require("./config/connect");
 
-
 /* logger morgan */
-
 app.use(logger("tiny"));
 
 /* json parser */
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 /* connect to db */
 Connect();
 
+/* Enable CORS */
+app.use(cors()); 
 
 /* routes */
 app.use("/api", userRouter);
-/* server start */
 
+/* server start */
 const PORT = process.env.PORT;
 app.listen(PORT, console.log(`server started on port ${PORT}`));
